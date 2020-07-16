00d17151d02d4e995d9f01b43d8ccf4ce2468482
Milan <milanvodak02@gmail.com>
2020-07-16 15:13:50 +0200

    Rework database views routing with a controller

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Employee;
-use App\Customer;
-use App\Order;
-use App\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,69 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/employees', function() {
-    $employees = Employee::select()->get();
-
-    return view('database', [
-        'tableName' => 'employees',
-        'keys' => [
-            'id',
-            'name',
-            'discord_id',
-            'tycoon_id',
-            'rank',
-            'trainee',
-            'joined_at',
-            'active',
-            'trailer',
-            'faction',
-            'email',
-            'management_rank'
-        ],
-        'data' => $employees
-    ]);
-});
-
-Route::get('customers', function() {
-    $customers = Customer::all();
-
-    return view('database', [
-        'tableName' => 'customers',
-        'keys' => ['id', 'discord_name', 'discord_id', 'tycoon_name', 'tycoon_id'],
-        'data' => $customers
-    ]);
-});
-
-Route::get('orders', function() {
-    $orders = Order::select()->get();
-
-    return view('database', [
-        'tableName' => 'orders',
-        'keys' => [
-            'id',
-            'timestamp',
-            'priority',
-            'status',
-            'progress',
-            'customer_id',
-            'worker_id',
-            'product_name',
-            'price_each',
-            'amount',
-            'discount',
-            'storage_id'
-        ],
-        'data' => $orders
-    ]);
-});
-
-Route::get('storages', function() {
-    $storages = Storage::select()->get();
-
-    return view('database', [
-        'tableName' => 'storages',
-        'keys' => ['id', 'name', 'fee', 'faction'],
-        'data' => $storages
-    ]);
-});
-
+Route::get('employees', 'DbViewController@employees');
+Route::get('customers', 'DbViewController@customers');
+Route::get('orders', 'DbViewController@orders');
+Route::get('storages', 'DbViewController@storages');
