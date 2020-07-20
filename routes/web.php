c93c782aaf50d025f474419477e6a25bc3637f82
Milan <milanvodak02@gmail.com>
2020-07-20 12:01:27 +0200

    Implement orders API
    Add API endpoints for interacting with orders table:
    POST /new
    GET /{order_id}
    PATCH /{order_id}/assign
    PATCH /{order_id}/progress
    PATCH /{order_id}/collection
    PATCH /{order_id}/complete
    PATCH /{order_id}/edit/priority
    PATCH /{order_id}/edit/amount
    PATCH /{order_id}/edit/discount
    PATCH /{order_id}/edit/storage
    PATCH /{order_id}/cancel
    
    Move database list routes to individual prefix groups.

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,34 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('employees', 'DbViewController@employees');
-Route::get('customers', 'DbViewController@customers');
-Route::get('orders', 'DbViewController@orders');
-Route::get('storages', 'DbViewController@storages');
+Route::prefix('employees')->group(function() {
+    Route::get('', 'DbViewController@employees');
+});
+
+Route::prefix('customers')->group(function() {
+    Route::get('', 'DbViewController@customers');
+});
+
+Route::prefix('orders')->group(function() {
+    Route::get('', 'DbViewController@orders');
+    Route::post('new', 'OrdersController@new')->middleware('exists:customer', 'valid:amount', 'exists:storage');
+
+    Route::group(['prefix' => '{order_id}', 'middleware' => 'exists:order'], function() {
+        Route::get('', 'OrdersController@get');
+        Route::patch('assign', 'OrdersController@assign')->middleware('exists:employee');
+        Route::patch('progress', 'OrdersController@progress')->middleware('valid:progress');
+        Route::patch('collection', 'OrdersController@collection');
+        Route::patch('complete', 'OrdersController@complete');
+        Route::prefix('edit')->group(function() {
+            Route::patch('priority', 'OrdersController@editPriority');
+            Route::patch('amount', 'OrdersController@editAmount')->middleware('valid:amount');
+            Route::patch('discount', 'OrdersController@editDiscount');
+            Route::patch('storage', 'OrdersController@editStorage')->middleware('exists:storage');
+        });
+        Route::patch('cancel', 'OrdersController@cancel');
+    });
+});
+
+Route::prefix('storages')->group(function() {
+    Route::get('', 'DbViewController@storages');
+});
