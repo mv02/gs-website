b5ba17167d6e9f27d05ddf2638f3b45635f886a6
Milan <milanvodak02@gmail.com>
2020-08-06 09:00:16 +0200

    Create 'cargoes' table

---
+++ database/migrations/2020_08_05_084602_create_cargoes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCargoesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cargoes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedMediumInteger('price');
+            $table->unsignedMediumInteger('limit');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cargoes');
+    }
+}
