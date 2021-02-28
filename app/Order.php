9ac6b77a928bdb6eb1930804f8c62a984ca7232d
Milan <milanvodak02@gmail.com>
2021-02-28 19:11:00 +0100

    Add several functions to the Order model
    - Functions to retrieve customer, grinder and storage
    - Attributes total price, progress percentage, and formatted price/amount/progress

--- app/Order.php
+++ app/Order.php
@@ -22,4 +22,47 @@ class Order extends Model
         'completed_at',
         'delivered_at'
     ];
+
+    function customer() {
+        return $this->belongsTo('App\User', 'customer_id');
+    }
+
+    function grinder() {
+        return $this->belongsTo('App\User', 'grinder_id');
+    }
+
+    function storage() {
+        return $this->belongsTo('App\Storage');
+    }
+
+    function getTotalPriceAttribute() {
+        $totalPrice = $this->amount * ($this->price_each + $this->storage->fee);
+        if ($this->priority) $totalPrice *= 1.15;
+        if ($this->discount) $totalPrice -= $totalPrice * $this->discount / 100;
+        return $totalPrice;
+    }
+
+    function getAmountStringAttribute() {
+        return number_format($this->amount, 0, '.', ',');
+    }
+
+    function getPriceEachStringAttribute() {
+        return '$' . number_format($this->price_each, 0, '.', ',');
+    }
+
+    function getTotalPriceStringAttribute() {
+        return '$' . number_format($this->total_price, 0, '.', ',');
+    }
+
+    function getDiscountStringAttribute() {
+        return "-{$this->discount}%";
+    }
+
+    function getProgressStringAttribute() {
+        return number_format($this->progress, 0, '.', ',');
+    }
+
+    function getProgressPercentageAttribute() {
+        return floor($this->progress / $this->amount * 100);
+    }
 }
