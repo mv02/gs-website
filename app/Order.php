18b7a0e9b3fa0fd5f492764391140ebc6f799678
Milan <milanvodak02@gmail.com>
2021-02-28 20:24:04 +0100

    Add a function to generate order embeds

--- app/Order.php
+++ app/Order.php
@@ -65,4 +65,45 @@ class Order extends Model
     function getProgressPercentageAttribute() {
         return floor($this->progress / $this->amount * 100);
     }
+
+    function getEmbedAttribute() {
+        $title = '\🚛 Order ' . $this->id;
+        if ($this->status == 'Completed') $color = 5025616;
+        else $color = $this->priority ? 16750592 : 240116;
+        $fields = [
+            ['name' => 'Order Information', 'value' =>  "**{$this->amount_string}x** {$this->product_name} @ {$this->storage->name}"],
+            ['name' => 'Total Price', 'value' => "{$this->total_price_string} ({$this->discount_string})"],
+            ['name' => 'Customer', 'value' => "**{$this->customer->name}** ({$this->customer->tycoon_id}) <@{$this->customer->discord_id}>"]
+        ];
+
+        switch ($this->status) {
+            case 'Queued':
+                $description = 'Press \✅ to assign this order to yourself.';
+                $timestamp = $this->created_at;
+                $footer = ['text' => $this->id];
+                break;
+
+            case 'In Progress':
+                $description = "\🕑 **In Progress** - <@{$this->grinder->discord_id}>";
+                $timestamp = $this->created_at;
+                $footer = ['text' => 'Placed at:'];
+                array_unshift($fields, ['name' => 'Progress', 'value' => "{$this->progress_string} / {$this->amount_string} ({$this->progress_percentage}%)"]);
+                break;
+
+            case 'Completed':
+                $description = "\✅ **Awaiting Collection** - Please contact <@{$this->grinder->discord_id}>!";
+                $timestamp = $this->completed_at;
+                $footer = ['text' => 'Completed at:'];
+                break;
+        }
+
+        return [
+            'title' => $title,
+            'description' => $description,
+            'timestamp' => $timestamp,
+            'color' => $color,
+            'footer' => $footer,
+            'fields' => $fields
+        ];
+    }
 }
