743b6ff87b08ebd29012a07d6ee321197b3c619a
Milan <milanvodak02@gmail.com>
2021-03-01 12:46:20 +0100

    Make all Cargo properties fillable

--- app/Cargo.php
+++ app/Cargo.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Cargo extends Model
 {
     public $table = 'cargoes';
+    public $fillable = [
+        'name',
+        'price',
+        'limit'
+    ];
 }
