3c1424a21d90c5f8afb17471417ea545b21a3734
Milan <milanvodak02@gmail.com>
2021-03-03 15:36:00 +0100

    Implement new orders API endpoints
    GET /orders
    GET /orders/active
    GET /orders/queued
    GET /orders/in-progress
    GET /orders/completed
    GET /orders/delivered
    GET /orders/{id}
    POST /orders
    PATCH /orders/{id}/grinder/{discord_id}
    PATCH /orders/{id}/progress/{progress}
    PATCH /orders/{id}/status
    PATCH /orders/{id}/edit
    PATCH /orders/{id}/reset

--- routes/api.php
+++ routes/api.php
@@ -18,21 +18,38 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:sanctum')->group(function() {
-    Route::prefix('/users')->group(function() {
+Route::prefix('/users')->middleware('auth:sanctum')->group(function() {
+    Route::get('', 'UserController@getUsers');
+    Route::post('', 'UserController@addUser');
+    Route::prefix('/employees')->group(function() {
+        Route::get('', 'UserController@getAllEmployees');
+        Route::get('/active', 'UserController@getActiveEmployees');
+        Route::get('/inactive', 'UserController@getInactiveEmployees');
+    });
+    Route::prefix('/{user_identifier}')->group(function() {
         Route::get('', 'UserController@getUsers');
-        Route::post('', 'UserController@addUser');
-        Route::prefix('/employees')->group(function() {
-            Route::get('', 'UserController@getAllEmployees');
-            Route::get('/active', 'UserController@getActiveEmployees');
-            Route::get('/inactive', 'UserController@getInactiveEmployees');
-        });
-        Route::prefix('/{user_identifier}')->group(function() {
-            Route::get('', 'UserController@getUsers');
-            Route::get('/hire', 'UserController@hireUser');
-            Route::get('/fire', 'UserController@fireUser');
-            Route::get('/promote', 'UserController@promoteUser');
-            Route::get('/demote', 'UserController@demoteUser');
-        });
+        Route::get('/hire', 'UserController@hireUser');
+        Route::get('/fire', 'UserController@fireUser');
+        Route::get('/promote', 'UserController@promoteUser');
+        Route::get('/demote', 'UserController@demoteUser');
+    });
+});
+
+Route::prefix('/orders')->middleware('auth:sanctum')->group(function() {
+    Route::get('', 'OrderController@getOrders');
+    Route::post('', 'OrderController@addOrder');
+    Route::get('/active', 'OrderController@getActiveOrders');
+    Route::get('/queued', 'OrderController@getQueuedOrders');
+    Route::get('/in-progress', 'OrderController@getInProgressOrders');
+    Route::get('/completed', 'OrderController@getCompletedOrders');
+    Route::get('/delivered', 'OrderController@getDeliveredOrders');
+    Route::prefix('/{order_id}')->group(function() {
+        Route::get('', 'OrderController@getOrders');
+        Route::patch('/grinder/{grinder_discord_id}', 'OrderController@setOrderGrinder');
+        Route::patch('/progress/{progress}', 'OrderController@setOrderProgress');
+        Route::patch('/status', 'OrderController@nextOrderStatus');
+        Route::patch('/edit', 'OrderController@editOrderData');
+        Route::patch('/cancel', 'OrderController@cancelOrder');
+        Route::patch('/reset', 'OrderController@resetOrder');
     });
 });
