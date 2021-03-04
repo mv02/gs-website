5facf466cedd5ad094cc95ace46a5e575af849ad
Milan <milanvodak02@gmail.com>
2021-03-04 18:49:50 +0100

    Create a User model authorization policy

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    public function before(User $user) {
+        if ($user->management) return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\User  $model
+     * @return mixed
+     */
+    public function view(User $user, User $model) {
+        return $user->id = $model->id;
+    }
+
+    public function viewEmployees(User $user) {
+        return $user->employee;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\User  $model
+     * @return mixed
+     */
+    public function update(User $user, User $model) {
+        return $user->id = $model->id;
+    }
+}
