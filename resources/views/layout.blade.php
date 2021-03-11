1ffda091c12b0092cefad32cdd25b37d9a9e9e41
Milan <milanvodak02@gmail.com>
2021-03-11 19:27:28 +0100

    Fix assets not being loaded properly

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/app.css">
-    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-    <script src="js/app.js"></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}" type="image/x-icon">
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <title>Grinding Satisfaction</title>
 </head>
